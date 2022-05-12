6ed2922f5a46275e03e87aa4208fb13fc008d710
Tima <02.timoha.0@gmail.com>
2022-05-12 22:59:29 +0300

    add map render on current postion when it allowed

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -11,18 +11,66 @@ function MainPage() {
 
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const [address, setAddress] = useState("");
+  const [adr1, setAdr1] = useState("");
+  const [adr2, setAdr2] = useState("");
+  const [userLat2, setUserLat2] = useState();
+  const [userLng2, setUserLng2] = useState();
   const [lng, setLng] = useState(30.3207);
   const [lat, setLat] = useState(59.9401);
-  const [zoom, setZoom] = useState(12);
+  const [zoom, setZoom] = useState(16);
+
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  function success(pos) {
+    const crd = pos.coords;
+    // console.log("Ваше текущее местоположение:");
+    // console.log(`Широта: ${crd.latitude}`);
+    // console.log(`Долгота: ${crd.longitude}`);
+    // console.log(`Плюс-минус ${crd.accuracy} метров.`);
+    setUserLat2(crd.latitude);
+    setUserLng2(crd.longitude);
+    setLat(crd.latitude);
+    setLng(crd.longitude);
+    if (!map.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [crd.longitude, crd.latitude],
+        zoom: zoom,
+      });
+    }
+  }
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
+    navigator.permissions &&
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (PermissionStatus) {
+          if (PermissionStatus.state === "denied") {
+            map.current = new mapboxgl.Map({
+              container: mapContainer.current,
+              style: "mapbox://styles/mapbox/dark-v10",
+              center: [30.3609, 59.9311],
+              zoom: 15,
+            });
+          } else {
+            if (map.current) return;
+            try {
+              navigator.geolocation.getCurrentPosition(success, error, options);
+            } catch (e) {
+              console.log(e.message);
+            }
+          }
+        });
   });
 
   useEffect(() => {
@@ -34,12 +82,36 @@ function MainPage() {
     });
   });
 
+  useEffect(() => {
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${mapboxgl.accessToken}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setAdr1(data.features[0]?.text ?? "");
+        setAdr2(data.features[0]?.address ?? "");
+        if (!adr1 && !adr2) {
+          setAddress("Can't define place");
+        }
+        setAddress(adr1 + " " + adr2);
+      });
+  }, [lat, lng, adr1, adr2, userLat2, userLng2]);
+
   return (
     <div className={styles.main}>
       <Header></Header>
       <div>
+        <button
+          onClick={() => {
+            map.current.flyTo({ center: [userLng2, userLat2] });
+          }}
+        >
+          Get position
+        </button>
         <div className={styles.sidebar}>
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} | Address: {address}
         </div>
         <div ref={mapContainer} className={styles.map} />
       </div>
